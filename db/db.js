99e587490aaea1b01004f5c62c43ea08adb50548
Max <ishchenkomaksym46@gmail.com>
2026-04-03 19:28:16 +0300

    Database connection

--- db/db.js
+++ db/db.js
@@ -3,9 +3,5 @@ import 'dotenv/config'
 
 /** @type {import('pg').Pool} */
 export const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    connectionString: process.env.DATABASE_URL
 });
